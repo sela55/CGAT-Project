7e1ccfe4aaf3d1f3c90a36adc6683809adbf6b37
Stav Elmashally <stavelmashally6@gmail.com>
2020-07-26 17:05:08 +0300

    Create Statistics Screen

---
+++ src/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Statistics extends React.Component{
+    render() {
+        return (
+            <div>Statistics Screen</div>
+        );
+    }
+}
+
+export default Statistics;
