87f89a680fa861c192fcce715c1077b050552b76
Stav Elmashally <stavelmashally6@gmail.com>
2020-08-05 10:00:05 +0300

    Added tableConfig

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -3,8 +3,32 @@ import MaterialTable from 'material-table';
 import './Statistics.css';
 import { API_DATA } from './api';
 
+const tableConfig = {
+  columns: [
+    { title: 'שם משתמש', field: 'userName' },
+    { title: 'הרשאות', field: 'permissions' },
+    { title: 'פעולה שביצע', field: 'operation' },
+    { title: 'ארגון', field: 'organization' },
+    { title: 'תפקיד', field: 'role' },
+    { title: 'תאריך', field: 'date' },
+  ],
+  tableOptions: {
+    search: true,
+    actionsColumnIndex: -1,
+    headerStyle: {
+      color: '#000',
+      fontSize: '16px',
+      fontWeight: 'bold',
+    },
+  },
+};
+
 class Statistics extends React.Component {
   state = { data: [], isLoading: true, selectedRow: null };
+
+  onRowClicked = (event, row) => {
+    this.setState({ selectedRow: row.tableData.id });
+  };
 
   componentDidMount() {
     // Will be an api call
@@ -18,29 +42,10 @@ class Statistics extends React.Component {
       <div className="ui container">
         <MaterialTable
           title="היסטוריית פעילות"
-          columns={[
-            { title: 'שם משתמש', field: 'userName' },
-            { title: 'הרשאות', field: 'permissions' },
-            { title: 'פעולה שביצע', field: 'operation' },
-            { title: 'ארגון', field: 'organization' },
-            { title: 'תפקיד', field: 'role' },
-            { title: 'תאריך', field: 'date' },
-          ]}
+          columns={tableConfig.columns}
           data={this.state.data}
-          onRowClick={(event, row) =>
-            this.setState({ selectedRow: row.tableData.id })
-          }
-          options={{
-            search: true,
-            actionsColumnIndex: -1,
-            headerStyle: { color: '#000', fontSize: '16px', fontWeight: 'bold' },
-            rowStyle: (rowData) => ({
-              backgroundColor:
-                this.state.selectedRow === rowData.tableData.id
-                  ? '#EEE'
-                  : '#FFF',
-            }),
-          }}
+          onRowClick={this.onRowClicked}
+          options={tableConfig.tableOptions}
           isLoading={this.state.isLoading}
         />
       </div>
