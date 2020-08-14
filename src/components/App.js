a2702afe61e23c6c2afc8f32e624bef0f6e9fdd8
Stav Elmashally <stavelmashally6@gmail.com>
2020-08-14 20:21:48 +0300

    Added api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ResponsiveGrid from './Dashboard/ResponsiveGrid';
-import '../node_modules/react-grid-layout/css/styles.css';
-import '../node_modules/react-resizable/css/styles.css';
+import '../../node_modules/react-grid-layout/css/styles.css';
+import '../../node_modules/react-resizable/css/styles.css';
 
 function App() {
   return (
