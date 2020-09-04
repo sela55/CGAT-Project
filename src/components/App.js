84b22e62ec7e3a3997c83fb0a40eb33eae2fa27b
Sela Shabtai <sela.shabtai@icloud.com>
2020-09-04 23:39:56 +0300

    added new libraries to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from "react";
-import ResponsiveGrid from "./Dashboard/ResponsiveGrid";
-import "../../node_modules/react-grid-layout/css/styles.css";
-import "../../node_modules/react-resizable/css/styles.css";
+import React from 'react';
+import ResponsiveGrid from './Dashboard/ResponsiveGrid';
+import '../../node_modules/react-grid-layout/css/styles.css';
+import '../../node_modules/react-resizable/css/styles.css';
+import NavBar from "./Menu/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from "./Menu/Menu";
+import CreateChart from "./Menu/CreateChart";
 
 function App() {
-  return (
-    <div style={{ height: "100vh", background: "rgb(237,241,247)" }}>
-      <ResponsiveGrid />
-    </div>
-  );
+    return (
+        <div style={{ height: '100vh' }}>
+            {/*<NavBar/>*/}
+            {/*<Menu/>*/}
+            {/*<CreateChart />*/}
+            <ResponsiveGrid/>
+        </div>
+    );
 }
 
 export default App;
